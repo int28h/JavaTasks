e21f20a2932612c53e221cf3dae820258a94824b
Elena Listratova <int28h@protonmail.com>
2017-09-14 00:23:30 +0400

    1 and 101

---
+++ src/list-reverse.java
@@ -0,0 +1,102 @@
+﻿/**
+ * Вам задан односвязный список в виде массива длины n: i-й элемент содержит ссылку на следующий элемент списка. Формально, в этой задаче список всегда начинается со значения 1, i-й элемент массива содержит значение равное следующему элементу списка после значения i. Если i-й элемент массива равен 0, то список заканчивается значением i.
+ * Например, если n=5, а массив имеет вид a=[5, 4, 0, 3, 2], то за значением 1 следует значение 5 (так как a[1]=5), затем следует значение 2 (так как a[5]=2) и так далее. Таким образом, заданный список имеет вид: 1 -> 5 -> 2 -> 4 -> 3.
+ * 
+ * Выведите заданный список в обратном порядке.
+ * 
+ * Входные данные
+ * В первой строке входных данных записано целое число n (1 <= n <= 100) — длина списка.
+ * Во второй строке записаны элементы массива a1, a2,..., an (0 <= ai <=n, ai != 1), где ai равно элементу, который следует в списке за элементом i или a[i]=0, если элемент i — последний элемент списка.
+ * Гарантируется, что массив a корректно задает некоторый список длины n, который начинается со значения 1.
+ * 
+ * Выходные данные
+ * Выведите элементы заданного списка в обратном порядке, то есть от последнего элементы списка к первому.
+ 
+ * === Пример 1 ===
+ * input.txt
+ * 5
+ * 5 4 0 3 2
+ * output.txt
+ * 3 4 2 5 1 
+ *
+ * === Пример 2 === 
+ * input.txt
+ * 3
+ * 3 0 2
+ * output.txt
+ * 2 3 1 
+ */
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class Main {
+	private static class Node{
+		Node next;
+		int number;
+	}
+	
+	private static Node head = new Node();
+
+    public static void main(String[] args) throws FileNotFoundException {    	    	
+    	Scanner in = new Scanner(new File("input.txt"));
+        int[] array = new int[in.nextInt()];
+        for(int i = 0; i < array.length; i++) {        	
+        	array[i] = in.nextInt();
+        }       
+        in.close();             
+        
+        Node first = new Node();
+        first.number = 1;
+        head.next = first;
+        Node current = first;
+        for(int i = 0; i < array.length; i++) {
+        	Node x = new Node();
+        	current.next = x;
+        	if (array[current.number - 1] == 0) {
+        		x.number = current.number - 1;
+        		break;
+        	}
+        	x.number = array[current.number - 1];        	
+        	current = x;
+        }
+        
+        FileOutputStream fos = new FileOutputStream("output.txt");
+        PrintStream out = new PrintStream(fos);
+        
+        //Так как автор задания не уточнил, каким  именно образом следует производить вывод перевернутого списка, ниже представлены два варианта решения.
+        
+        //первый способ
+        StringBuilder s = new StringBuilder();
+        current = first;
+        for(int i = 0; i < array.length; i++) {
+        	if(i == 0) {
+        		s.append(current.number);
+        		current = current.next;
+        	} else {
+        	s.append(" ").append(current.number);
+        	current = current.next;
+        	}
+        }
+        out.print(s.reverse());
+        
+        //второй способ
+        int[] reverse = new int[array.length];
+        current = head;
+        for(int i = 0; i < array.length; i++) {
+        	current = current.next;
+        	reverse[i] = current.number;
+        }
+        for(int i = array.length - 1; i >= 0; i--) {
+        	if (i == array.length) {
+        		out.print(reverse[i]);
+        	} else {
+        		out.print(" " + reverse[i]);
+        	}
+        }
+        out.close();
+    }
+}
---
+++ src/sortland.java
@@ -0,0 +1,57 @@
+/**
+ * Владелец графства Сортлэнд, граф Бабблсортер, решил познакомиться со своими подданными. Число жителей в графстве нечетно и составляет n, где n  может быть достаточно велико, поэтому граф решил ограничиться знакомством с тремя представителями народонаселения: с самым бедным жителем, с жителем, обладающим средним достатком, и с самым богатым жителем.
+ * Согласно традициям Сортлэнда, считается, что житель обладает средним достатком, если при сортировке жителей по сумме денежных сбережений он оказывается ровно посередине. Известно, что каждый житель графства имеет уникальный идентификационный номер, значение которого расположено в границах от единицы до n. Информация о размере денежных накоплений жителей хранится в массиве M таким образом, что сумма денежных накоплений жителя, обладающего идентификационным номером i, содержится в ячейке M[i].
+ * Помогите секретарю графа мистеру Свопу вычислить идентификационные номера жителей, которые будут приглашены на встречу с графом.
+ * 
+ * Формат входного файла
+ * Первая строка входного файла содержит число жителей n(3 <= n <= 9999, n нечетно). Вторая строка содержит описание массива , состоящее из положительных вещественных чисел, разделенных пробелами. Гарантируется, что все элементы массива различны, а их значения имеют точность не более двух знаков после запятой и не превышают 10^6.
+ * 
+ * Формат выходного файла
+ * В выходной файл выведите три целых положительных числа, разделенных пробелами — идентификационные номера беднейшего, среднего и самого богатого жителей Сортлэнда. 
+ */
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner in = new Scanner(new File("input.txt"));
+        Float[][] array = new Float[in.nextInt()][2];
+        for(int i = 0; i < array.length; i++) {
+        	for(int j = 0; j < 1; j++) {
+        		array[i][j] = Float.valueOf(in.next());
+        		array[i][j+1] = (float) (i + 1);
+        	}
+        }
+        in.close();
+        for(int j = 0; j < array.length; j++){
+	            int i = j - 1;
+	            while(i >= 0 && array[i][0] > array[i+1][0]) {
+	                Float temp = array[i][0];
+	                Float temp1 = array[i][1];
+	                array[i][0] = array[i+1][0];
+	                array[i][1] = array[i+1][1];
+	                array[i+1][0] = temp;
+	                array[i+1][1] = temp1;
+	                i--;
+	            }
+        } 
+        for(int i = 0; i < array.length; i++) {
+        	for(int j = 0; j < 2; j++) {        		
+        		System.out.print(array[i][j] + " ");
+        	}
+        	System.out.println();
+        }
+        float poorIndex = array[0][1];
+        float averageIndex = array[array.length/2][1];
+        float richIndex = array[array.length-1][1];
+        System.out.print((int) poorIndex + " " + (int) averageIndex + " " + (int) richIndex);
+        FileOutputStream fos = new FileOutputStream("output.txt");
+        PrintStream out = new PrintStream(fos);
+        out.print((int)poorIndex + " " + (int)averageIndex + " " + (int)richIndex);
+        out.close();
+    }
+}
